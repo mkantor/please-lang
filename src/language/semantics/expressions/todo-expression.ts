9d3bba2cf566eab714501134e7971b0e14a59d69
Matt Kantor <the.matt.kantor@gmail.com>
2026-09-18 07:21:14 -0400

    Infer types for `@check` and `@todo`
    Neither had a branch in `inferType`, so they were inferred as raw
    expression objects. This isn't currently user-visible, but I'll be
    adding hover type info soon and then it will be.

--- src/language/semantics/expressions/todo-expression.ts
+++ src/language/semantics/expressions/todo-expression.ts
@@ -1,5 +1,19 @@
-import type { ObjectNode } from '../object-node.js'
+import type { Either } from '@matt.kantor/either'
+import either from '@matt.kantor/either'
+import type { ElaborationError } from '../../errors.js'
+import { isObjectNode, withProperty, type ObjectNode } from '../object-node.js'
+import type { SemanticGraph } from '../semantic-graph.js'
 
 export type TodoExpression = ObjectNode & {
   readonly 0: '@todo'
 }
+
+export const readTodoExpression = (
+  node: SemanticGraph,
+): Either<ElaborationError, TodoExpression> =>
+  isObjectNode(node) && node[0] === '@todo' ?
+    either.makeRight(withProperty(node, '0', '@todo'))
+  : either.makeLeft({
+      kind: 'invalidExpression',
+      message: 'not a `@todo` expression',
+    })
